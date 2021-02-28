fb3450b098638fbc3bb82b57ef1fbc69a77e59e4
Hikaruweng <hikaruweng@Hikaruwengs-MacBook-Pro.local>
2021-02-28 16:39:42 +0700

    Finish Part4 Lab4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Part4Lab3
+# Part4Lab4
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.1.
 
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-post-list',
@@ -8,13 +10,37 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PostListComponent implements OnInit {
   posts: any[];
-  constructor(private httpClient: HttpClient) {}
+  form: FormGroup;
+
+  constructor(private httpClient: HttpClient, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      userId: '',
+      title: '',
+      body: '',
+    });
+  }
 
   ngOnInit(): void {
+    this.loadPost();
+  }
+
+  loadPost() {
+    this.posts = [];
     this.httpClient
       .get('https://jsonplaceholder.typicode.com/posts')
       .subscribe((result) => {
         this.posts = result as any[];
+      });
+  }
+
+  addPost() {
+    const newPost = this.form.value;
+    this.httpClient
+      .post('https://jsonplaceholder.typicode.com/posts', newPost)
+      .subscribe((result) => {
+        this.form.reset();
+        alert('Add Post Success !');
+        this.loadPost();
       });
   }
 }
